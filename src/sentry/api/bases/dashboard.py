9a16ee7366c975432fb25c033371c71030aef1e0
Lauryn Brown <lauryndbrown@gmail.com>
2019-01-09 11:40:22 -0800

    Added new changes to dashboards.

--- src/sentry/api/bases/dashboard.py
+++ src/sentry/api/bases/dashboard.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import
 from rest_framework import serializers
+from sentry.api.serializers.rest_framework.json import JSONField
+from sentry.api.fields.user import UserField
 
 
 class DashboardSerializer(serializers.Serializer):
     title = serializers.CharField(required=True)
+    owner = UserField(required=True)
+    data = JSONField(
+        required=False,
+    )
